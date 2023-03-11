9c63266331b65ee43e52c8557687306d59922950
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-03-11 11:08:06 -0500

    add generated text to git ignore

--- aiFeeder.py
+++ aiFeeder.py
@@ -18,13 +18,4 @@ textFromJPG = pytesseract.image_to_string(Image.open('data/testConvert.jpg'))
 #wite text from the OCR to a text file 
 text_file = open("text_from_JPG.txt", "w")
 n = text_file.write(textFromJPG)
-text_file.close()
-
-# list_files = subprocess.run(["ls", "-l"])
-# print("The exit code was: %d" % list_files.returncode)
-
-# useless_cat_call = subprocess.run(["cat"], 
-#                                   stdout=subprocess.PIPE, 
-#                                   text=True, 
-#                                   input="Hello from the other side")
-# print(useless_cat_call.stdout)  
+text_file.close() 
